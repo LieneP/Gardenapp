0dc33876785d49c96cc291df3d06198e9fa6b203
liene <liene.strauja@gmail.com>
2021-09-01 20:34:55 +0300

    2nd refresh

--- src/Flora.java
+++ src/Flora.java
@@ -1,2 +1,59 @@
 public class Flora {
+
+    protected String englishName;
+    protected String latinName;
+    protected String type;
+    protected int floraID;
+
+    public Flora() {
+    }
+
+    public Flora(String englishName, String latinName, String type, int floraID) {
+        this.englishName = englishName;
+        this.latinName = latinName;
+        this.type = type;
+        this.floraID = floraID;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getLatinName() {
+        return latinName;
+    }
+
+    public void setLatinName(String latinName) {
+        this.latinName = latinName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getFloraID() {
+        return floraID;
+    }
+
+    public void setFloraID(int floraID) {
+        this.floraID = floraID;
+    }
+
+    @Override
+    public String toString() {
+        return "Flora{" +
+                "englishName='" + englishName + '\'' +
+                ", latinName='" + latinName + '\'' +
+                ", type='" + type + '\'' +
+                ", floraID=" + floraID +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome in the garden app!");
+        System.out.println("======== WELCOME TO GARDEN APPLICATION! +++++++++");
+        System.out.println("Please choose next step:");
+        System.out.println("1 - Show advice what to plant.");
+        System.out.println("2 - Display information about available plants.");
+        System.out.println("0 - Exit");
+
     }
 }
