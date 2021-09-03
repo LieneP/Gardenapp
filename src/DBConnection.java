7bae6847955400616db3237264272c9792b7c474
annab <annabo@inbox.lv>
2021-09-03 19:37:34 +0300

    DBConnection class added

---
+++ src/DBConnection.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+public class DBConnection {
+
+    private Connection conn;
+
+    public DBConnection() {
+
+        try {
+            String dbUrl = "jdbc:sqlite:Flora.db";
+            conn = DriverManager.getConnection(dbUrl);
+
+            if (conn != null) {
+                DatabaseMetaData databaseMetaData = conn.getMetaData();
+                System.out.println("Connected to " + databaseMetaData.getDatabaseProductName() + " " + databaseMetaData.getDatabaseProductVersion());
+
+                //Creating table
+                Statement statement = conn.createStatement();
+                String sqlStatement =
+                        " CREATE TABLE IF NOT EXISTS Flora" +
+                                " (floraID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                "latinName TEXT NOT NULL, " +
+                                "latvianName TEXT NOT NULL, " +
+                                "type TEXT NOT NULL, " +
+                                "soil TEXT NOT NULL, " +
+                                "light TEXT NOT NULL," +
+                                "height TEXT NOT NULL)";
+
+                statement.execute(sqlStatement);
+
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            System.out.println("Database issues" + exception);
+        }
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DBConnection Floradb = new DBConnection();
+
         System.out.println("======== WELCOME TO GARDEN APPLICATION! +++++++++");
         System.out.println("Please choose next step:");
         System.out.println("1 - Show advice what to plant.");
